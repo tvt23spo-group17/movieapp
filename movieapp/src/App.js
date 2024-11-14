380295b2b68ebc60219028d43256efc6cca12780
LasseTrs <156923845+LasseTrs@users.noreply.github.com>
2024-11-14 16:31:35 +0200

    Add files via upload

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+ const [areas, setAreas] = useState([]);
+  const getTheatres = (xml) => {
+    const parser = new DOMParser()
+    const xmlDoc = parser.parseFromString(xml,'application/xml')
+    const root = xmlDoc.children
+const theatres = root[0].children
+const tempAreas = []
+    for (let i = 0;i <theatres.length;i++) {
+     // console.log(theatres[i].children[1].innerHTML)
+      tempAreas.push(
+        {"id": theatres[i].children[0].innerHTML,
+        "name": theatres[i].children[1].innerHTML
+      }
+      )
+    }
+    setAreas(tempAreas);
+    console.log(tempAreas)
+  }
+
+
+  useEffect(() => {
+    fetch('https://www.finnkino.fi/xml/TheatreAreas/')
+    .then(response => response.text())
+    .then(xml => {
+      getTheatres(xml)
+    })
+     .catch(error => {
+      console.log(error)
+     })
+    }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>finnkino Teatterit</h1>
+    <select>
+    {areas.map((area) => (
+          <option key={area.id} value={area.id}>
+            {area.name}
+          </option>
+        ))}
+    </select>
+    <select>
+
+    </select>
     </div>
   );
 }

21d5b4fbca406f8219e47a4c3ad40d38ba75c778
LasseTrs <156923845+LasseTrs@users.noreply.github.com>
2024-11-15 18:50:59 +0200

    Oma irrallaan oleva aikatauluhaku
    Hakee meneillään olevan päivän oulun teatterin näytökset

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+
 
 function App() {
+
+  const [schedules, setSchedules] = useState([]);
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+    try {
+    const response = await fetch('https://www.finnkino.fi/xml/Schedule/?area=1018'); //oulu valittu teatteriksi
+    const xmlText = await response.text(); 
+   const parser = new DOMParser();
+   const xmlDoc = parser.parseFromString(xmlText, 'application/xml');
+    const shows = xmlDoc.getElementsByTagName('Show');
+    const schedulesData = Array.from(shows).map((show) => {
+   const title = show.getElementsByTagName('Title')[0]?.textContent; //lehvan nimi
+    const eventTime = show.getElementsByTagName('dttmShowStart')[0]?.textContent; //näytöksen alku
+   return { title, eventTime };
+    });
+   setSchedules(schedulesData);
+    console.log(schedulesData)
+    } catch (error) {
+    console.error('error', error);
+    }
+    };
+    fetchSchedule();
+  }, []);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+<h1>Finnkino Oulu lehvat</h1>
+  <ul>
+{schedules.length > 0 ? (
+schedules.map((schedules, index) => (
+<li key={index}>
+<p>{schedules.title}</p> - {new Date(schedules.eventTime).toLocaleString()}
+ </li>
+  ))
+  ) : (
+  <li></li>
+ )}
+   </ul>
+ </div>
   );
 }
 
